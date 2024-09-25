c0a8e8b9e0675c0fdbe345abd94f12523eac151a
Roedeer0808 <mingeon2508@naver.com>
2024-09-25 21:45:30 +0900

    2024-9-25 3차시 (별 움직여보기, GotoXY 뜯어보기)

--- Test/Source.c
+++ Test/Source.c
@@ -34,65 +34,145 @@
 // 
 // }
 
+// #include <stdio.h>
+// #include <Windows.h	>
+// #include <conio.h>
+// #define LEFT 75
+// #define RIGHT 77
+// #define UP 72
+// #define DOWN 80
+// #define ARROW 224
+// void GotoXY(int x, int y) {
+// 	COORD Pos;
+// 	Pos.X = x;
+// 	Pos.Y = y;
+// 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+// }
+// void CursorView(char show)
+// {
+// 	CONSOLE_CURSOR_INFO ConsoleCursor;
+// 	ConsoleCursor.bVisible = show;
+// 	ConsoleCursor.dwSize = 1;
+// 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ConsoleCursor);
+// }
+// int x = 30;
+// int y = 7;
+// int main(void) {
+// 
+// 	CursorView(0);
+// 	GotoXY(x, y);
+// 	printf("★");
+// 	while (1) {
+// 		if (_kbhit()) {
+// 			int nkey = _getch();
+// 			if (nkey == ARROW) {
+// 				nkey = _getch();
+// 				switch (nkey) {
+// 				case UP:
+// 					system("cls");
+// 					GotoXY(x, --y);
+// 					printf("★");
+// 					break;
+// 				case LEFT:
+// 					system("cls");
+// 					GotoXY(x = x - 2, y);
+// 					printf("★");
+// 					break;
+// 				case RIGHT:
+// 					system("cls");
+// 					GotoXY(x = x + 2, y);
+// 					printf("★");
+// 					break;
+// 				case DOWN:
+// 					system("cls");
+// 					GotoXY(x, ++y);
+// 					printf("★");
+// 					break;
+// 
+// 				}
+// 			}
+// 		}
+// 	}
+// 
+// 	return 0;
+// }
+
+// #include <stdio.h>
+// #include <Windows.h	>
+// void GotoXY(int x, int y) {
+//     COORD Pos;
+//     Pos.X = x;
+//     Pos.Y = y;
+//     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+// }
+// 
+// int main(void) {
+//     GotoXY(0, 0);
+//     printf("어디에 출력되나?\n");
+// 
+//     return 0;
+// }
+
+
 #include <stdio.h>
-#include <Windows.h	>
+#include <windows.h>
 #include <conio.h>
+
 #define LEFT 75
 #define RIGHT 77
 #define UP 72
 #define DOWN 80
-#define ARROW 224
-void GotoXY(int x, int y) {
-	COORD Pos;
-	Pos.X = x;
-	Pos.Y = y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+#define ARROWS 224
+
+// #define SPACEBAR 32
+
+void GotoXY(int x, int y)
+{
+    COORD Pos;
+    Pos.X = x;
+    Pos.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
+
 void CursorView(char show)
 {
-	CONSOLE_CURSOR_INFO ConsoleCursor;
-	ConsoleCursor.bVisible = show;
-	ConsoleCursor.dwSize = 1;
-	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ConsoleCursor);
+    CONSOLE_CURSOR_INFO ConsoleCursor;
+    ConsoleCursor.bVisible = show;
+    ConsoleCursor.dwSize = 1;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ConsoleCursor);
 }
-int x = 30;
-int y = 7;
+
+int x = 4;
+int y = 8;
+
 int main(void) {
-
-	CursorView(0);
-	GotoXY(x, y);
-	printf("★");
-	while (1) {
-		if (_kbhit()) {
-			int nkey = _getch();
-			if (nkey == ARROW) {
-				nkey = _getch();
-				switch (nkey) {
-				case UP:
-					system("cls");
-					GotoXY(x, --y);
-					printf("★");
-					break;
-				case LEFT:
-					system("cls");
-					GotoXY(x = x - 2, y);
-					printf("★");
-					break;
-				case RIGHT:
-					system("cls");
-					GotoXY(x = x + 2, y);
-					printf("★");
-					break;
-				case DOWN:
-					system("cls");
-					GotoXY(x, ++y);
-					printf("★");
-					break;
-
-				}
-			}
-		}
-	}
-
-	return 0;
+    CiursorView(0);
+    GotoXY(x, y);
+    pritnf("◇");
+    while (1)
+    {
+        int nkey = _kbhit();
+        if(nkey==ARROWS)
+        { 
+            switch (nkey)
+            {
+            case UP:
+                //syetem("cls");
+                GotoXY(x, y++);
+                break;
+            case DOWN:
+                //syetem("cls");
+                GotoXY(x, y--);
+                break;
+            case LEFT:
+                //syetem("cls");
+                GotoXY(x--, y);
+                break;
+            case RIGHT:
+                //syetem("cls");
+                GotoXY(x++, y);
+                break;
+            }
+        }
+    }
 }
